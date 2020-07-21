5592a0def1e8f965c8747c4ac9081bfb060f76bc
Dominique K Kitchenakow <domikitchen@users.noreply.github.com>
2020-07-20 20:43:12 -0500

    completed mvp

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ export default function App() {
     fetchShow()
       .then(res => {
         console.log(res)
-        setShow(res.data);
-        setSeasons(formatSeasons(res.data._embedded.episodes));
+        setShow(res);
+        setSeasons(formatSeasons(res._embedded.episodes));
       })
   }, []);
 
